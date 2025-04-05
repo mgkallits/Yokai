4b547087ebc22c530a49131319ba664764128b0d
mgkallits <mgkallits@gmail.com>
2025-04-05 23:39:01 +0300

    Changed settings.

--- modules/user/home/services/mako.nix
+++ modules/user/home/services/mako.nix
@@ -1,34 +1,44 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
 {
   services.mako = {
     enable = true;
 
     # Explicitly set the font with high priority
-    # font = lib.mkForce "monospace 10";
-
+    font = lib.mkForce "ComicShannsMono Nerd Font 12";
     # Other mako configurations...
     actions = true;
     anchor = "top-right";
-    borderRadius = 5;
-    borderSize = 1;
-    defaultTimeout = 5000;
+    borderRadius = 4;
+    borderSize = 2;
+    defaultTimeout = 7000;
     groupBy = "app-name,urgency";
-    height = 150;
-    iconPath = "/usr/share/icons/hicolor:/usr/share/pixmaps";
+    height = 250;
+    # iconPath = "/usr/share/icons/hicolor:/usr/share/pixmaps";
     icons = true;
     ignoreTimeout = false;
     layer = "overlay";
     margin = "10,20,5";
     markup = true;
-    maxIconSize = 64;
+    maxIconSize = 96;
     maxVisible = 5;
     output = "";
     padding = "5";
+    # progressColor = lib.mkForce "over #ffff";
     sort = "-time";
     width = 300;
   };
 
+  home.file.".config/mako/colors-mako".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.xdg.cacheHome}/wal/colors-mako";
+
+
   home.packages = with pkgs; [
-    libnotify  # Provides notify-send
+    libnotify # Provides notify-send
   ];
 }
