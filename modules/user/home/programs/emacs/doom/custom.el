32a8f299e357a8c61506c8e29691717d0b6530e5
mgkallits <mgkallits@gmail.com>
2025-04-05 21:08:54 +0300

    Modified the config to only import my dotfiles of './doom'.

--- modules/user/home/programs/emacs/doom/custom.el
+++ modules/user/home/programs/emacs/doom/custom.el
@@ -7,7 +7,7 @@
  '(custom-safe-themes
    '("2035a16494e06636134de6d572ec47c30e26c3447eafeb6d3a9e8aee73732396"
      "8a379e7ac3a57e64de672dd744d4730b3bdb88ae328e8106f95cd81cbd44e0b6" default))
- '(package-selected-packages nil))
+ '(package-selected-packages '(@ vterm)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
