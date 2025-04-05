2c980a894210be594a82db4834012b9308c453ee
mgkallits <mgkallits@gmail.com>
2025-04-05 23:12:28 +0300

    Restructured and added host-specific imports.

--- modules/system/programs/jellyfin.nix
+++ modules/system/programs/jellyfin.nix
@@ -1,24 +1,32 @@
-{ pkgs, username, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  hostname,
+  username,
+  ...
+}:
 
 {
+  config = lib.mkIf (hostname == "kitsune") {
 
-  environment.systemPackages = with pkgs; [
-    # jellyfin
-    # jellyfin-web
-    # jellyfin-ffmpeg
-    # jellyfin-media-player
-  ];
+    environment.systemPackages = with pkgs; [
+      # jellyfin
+      # jellyfin-web
+      # jellyfin-ffmpeg
+      # jellyfin-media-player
+    ];
 
-  # Allow Jellyfin to read external drives
-  services.jellyfin = {
-    enable = false;
-    openFirewall = true;
-    user = "${username}"; 
-    # IMPORTANT: dont forget to run sudo chown -R /var/lib/jellyfin in order to allow it to
-    # see external drives mounted
+    # Allow Jellyfin to read external drives
+    services.jellyfin = {
+      enable = false;
+      openFirewall = true;
+      user = "${username}";
+      # IMPORTANT: dont forget to run sudo chown -R /var/lib/jellyfin in order to allow it to
+      # see external drives mounted
+    };
+
+    # === Acceleration for jellyfin ===
+    # Enable vaapi on
   };
-
-  # === Acceleration for jellyfin ===
-  # Enable vaapi on 
-
 }
