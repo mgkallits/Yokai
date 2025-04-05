e41cf2d795406ca770ccedc935a5a0f111db5c16
mgkallits <mgkallits@gmail.com>
2025-04-05 22:52:24 +0300

    Restructured, made them load depending on host specific.

--- modules/system/hardware/openrgb.nix
+++ modules/system/hardware/openrgb.nix
@@ -1,23 +1,24 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, hostname, ... }:
 
 {
-  
+
   # IMPORTANT: Please do note that installing this package by itself will lead to
-  # udev rules not being set up correctly. It is recommended to have both 
+  # udev rules not being set up correctly. It is recommended to have both
   # services.hardware.openrgb.enable = true; and the package installed (either
   # openrgb or openrgb-with-all-plugins)
 
+  config = lib.mkIf (hostname == "tanuki") {
+    environment.systemPackages = [
+      pkgs.openrgb
+      # pkgs.openrgb-with-all-plugins
+    ];
 
-  environment.systemPackages = [
-    pkgs.openrgb
-    # pkgs.openrgb-with-all-plugins 
-  ];
-
-  services.hardware.openrgb = {
-    enable = true;
-  #   package = pkgs.openrgb-with-all-plugins;
-  #   motherboard = "amd";
-  #   server.port = 6742;
+    services.hardware.openrgb = {
+      enable = true;
+      #   package = pkgs.openrgb-with-all-plugins;
+      #   motherboard = "amd";
+      #   server.port = 6742;
+    };
   };
 
 }
--- modules/system/hardware/ryzen-smu.nix
+++ modules/system/hardware/ryzen-smu.nix
@@ -9,26 +9,34 @@
 # Such use is not covered under any AMD product warranty and may also void
 # your board or system manufacturer's warranty. Proceed at your own risk.
 
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  hostname,
+  ...
+}:
 
 {
-  # Enable the Ryzen SMU kernel module to allow communication with the SMU.
-  hardware.cpu.amd.ryzen-smu.enable = true;
+  config = lib.mkIf (hostname == "kitsune") {
+    # Enable the Ryzen SMU kernel module to allow communication with the SMU.
+    hardware.cpu.amd.ryzen-smu.enable = true;
 
-  # Define a systemd service to automate the undervolting process.
-  # This service performs two tasks:
-  # 1. Clones the Ryzen 5800X3D undervolting repository if it doesn't already exist.
-  # 2. Applies the specified undervolting offset to all CPU cores.
-  systemd.services.ryzen-undervolt = {
-    description = "Clone Ryzen 5800X3D Repository and Apply Undervolting";
-    wantedBy = [ "multi-user.target" ]; # Start the service on boot
-    after = [ "network.target" ]; # Ensure the service starts after networking is available
+    # Define a systemd service to automate the undervolting process.
+    # This service performs two tasks:
+    # 1. Clones the Ryzen 5800X3D undervolting repository if it doesn't already exist.
+    # 2. Applies the specified undervolting offset to all CPU cores.
+    systemd.services.ryzen-undervolt = {
+      description = "Clone Ryzen 5800X3D Repository and Apply Undervolting";
+      wantedBy = [ "multi-user.target" ]; # Start the service on boot
+      after = [ "network.target" ]; # Ensure the service starts after networking is available
 
-    serviceConfig = {
-      Type = "oneshot"; # Run the service once at startup
-      ExecStartPre = "${pkgs.bash}/bin/bash -c 'if [ ! -d /home/mgkallits/Repos/Ryzen-5800x3d-linux-undervolting ]; then mkdir -p /home/mgkallits/Repos && git clone https://github.com/svenlange2/Ryzen-5800x3d-linux-undervolting.git /home/mgkallits/Repos/Ryzen-5800x3d-linux-undervolting; fi'";
-      ExecStart = "${pkgs.python3}/bin/python3 /home/mgkallits/Repos/Ryzen-5800x3d-linux-undervolting/ruv.py -c 8 -o -30"; # Apply undervolting offset (-30 in this case)
-      User = "root"; # Run as root (required for SMU access)
+      serviceConfig = {
+        Type = "oneshot"; # Run the service once at startup
+        ExecStartPre = "${pkgs.bash}/bin/bash -c 'if [ ! -d /home/mgkallits/Repos/Ryzen-5800x3d-linux-undervolting ]; then mkdir -p /home/mgkallits/Repos && git clone https://github.com/svenlange2/Ryzen-5800x3d-linux-undervolting.git /home/mgkallits/Repos/Ryzen-5800x3d-linux-undervolting; fi'";
+        ExecStart = "${pkgs.python3}/bin/python3 /home/mgkallits/Repos/Ryzen-5800x3d-linux-undervolting/ruv.py -c 8 -o -30"; # Apply undervolting offset (-30 in this case)
+        User = "root"; # Run as root (required for SMU access)
+      };
     };
   };
 }
