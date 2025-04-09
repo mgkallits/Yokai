2c0fa4affac2528d0326f6e9f9b918de294536cd
mgkallits <mgkallits@gmail.com>
2025-04-09 14:53:35 +0300

    Refined explainations.

--- settings.nix
+++ settings.nix
@@ -1,6 +1,8 @@
 {
 
-  # === General custom variables that configure the System ===
+  # === Custom Variables & Options ===
+
+  # == Variables ==
 
   # User and host information
   username = "mgkallits"; # Your username
@@ -11,7 +13,7 @@
   # Localization settings
   timezone = "Europe/Athens"; # Time zone configuration
   locale = "en_US.UTF-8"; # Default locale for system messages
-  extralocale = "el_GR.UTF-8";
+  extralocale = "el_GR.UTF-8"; # -> Currently unused.
 
   # Other apps
   editor = "vim";
@@ -21,14 +23,25 @@
   browser = "brave";
   gitUsername = "mgkallits";
   gitEmail = "mgkallits@gmail.com";
+  
+  # == Custom Options/Features ==
+
   display-manager = ""; # one of: "gdm", "" -> no display-manager meaning you have to log in using tty or a script
-  bleeding-edge = false; # whether to enable hyprland and other programs latest features by getting the packages staight from github instead of nixpkgs.
+  bleeding-edge = false; # whether to enable hyprland's, swww's and other program's latest features by pulling and building them from source
+  hyprswitch = true; # whether to enable hyprswitch.
+  # TODO: the rest
   mount-external-drives = true; # wehther to mound my external hard drives.
   secure-boot = true; # wether to enable/disable secure boot with lanzaboote.
   doomemacs = true; # whether to enable doom-emacs.
-  hyprswitch = true; # whether to enable hyprswitch.
+  
+  # NOTE: do i need this ??
+  # useuwsm = true; # Whether to enable universal waylans service manager.
 
-  # WARNING: System state version CAUTION! Do not change after initial setup (or else? TODO: improve this)
-  version = "24.11";
+  
+  # ~~ State Version Variable ~~ 
+
+  # WARNING: State Version for both System and Home-Manager
+  version = "24.05"; # CAUTION! Do not change after initial setup. See ./modules/system/general/INFO.md (TODO: improve this)
+
 
 }
