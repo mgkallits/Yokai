42262b940368cafa7a5336922c76e157310d6350
mgkallits <mgkallits@gmail.com>
2026-02-19 16:02:22 +0200

    Added some font families.

--- modules/system/general/fonts.nix
+++ modules/system/general/fonts.nix
@@ -5,21 +5,43 @@
     fontDir.enable = true;
     packages =
       (with pkgs.nerd-fonts; [
+        # Primary coding fonts (with Nerd Font symbols)
+        # NOTE: used the Font Name not the Original Font Name and Repository !!
         jetbrains-mono
-        caskaydia-mono # used the Font Name not the Original Font Name and Repository !!
+        caskaydia-mono
         comic-shanns-mono
         commit-mono
+        fira-code
+        symbols-only
       ])
       ++ (with pkgs; [
+
+        source-sans
+        source-sans-pro
+
+        # Unicode/Emoji coverage
         noto-fonts
-        noto-fonts-emoji
         noto-fonts-cjk-sans
         noto-fonts-color-emoji
+        symbola # Critical for ornamental dingbats
+        unifont # Last-resort fallback
+
+        # UI/Display fonts
         material-icons
         font-awesome
         atkinson-hyperlegible
         roboto
-        newcomputermodern # Doom Emacs
+        newcomputermodern
+        et-book
+        eb-garamond
+        libertinus
+        cardo
+        vollkorn
+        roboto-slab
+        alegreya
+        garamond-libre
+        junicode
+        comic-mono
       ]);
   };
 }
